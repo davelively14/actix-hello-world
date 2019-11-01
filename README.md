4187159c9e588f5d0a458d17e2d052215751d750
Dave Lively <davelively14@gmail.com>
2019-10-31 21:04:44 -0400

    Added macros

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 - `cargo build`
 - `cargo run`
-- Visit http://localhost.com:8080
+- Visit http://localhost.com:8088
+- Visit http://localhost.com:8088/basic
+- Visit http://localhost.com:8088/macros
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body("Good luck!")
 }
 
 fn index2() -> impl Responder {
-    HttpResponse::Ok().body("Hello world again!")
+    HttpResponse::Ok().body("Hello world, basic!")
+}
+
+#[get("/macros")]
+fn index3() -> impl Responder {
+    HttpResponse::Ok().body("This is with macros!")
 }
 
 fn main() {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
-            .route("/again", web::get().to(index2))
+            .route("/basic", web::get().to(index2))
+            .service(index3)
     })
     .bind("127.0.0.1:8088")
     .unwrap()
